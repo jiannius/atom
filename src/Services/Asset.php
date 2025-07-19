1502edfc32f0e11c39b5351291be1fabf13767a2
TJ <tj@jiannius.com>
2025-07-19 20:51:23 +0800

    rf: use laravel-vite-plugin for build

--- src/Services/Asset.php
+++ src/Services/Asset.php
@@ -2,6 +2,7 @@
 
 namespace Jiannius\Atom\Services;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 class Asset
@@ -18,7 +19,9 @@ class Asset
     public function version($name): string
     {
         $manifest = json_decode(file_get_contents(__DIR__.'/../../dist/manifest.json'), true);
-        $hash = $manifest[$name] ?? '';
+        $data = collect($manifest)->where(fn ($value, $key) => str($key)->is('*/'.$name))->values()->first();
+        $file = data_get($data, 'file');
+        $hash = str($file)->remove('assets/')->remove('atom-')->remove('.css')->remove('.js')->toString();
 
         return $hash;
     }
@@ -31,12 +34,10 @@ class Asset
             'editor.js' => 'text/javascript',
         ] as $script => $type) {
             Route::get("/atom/{$script}", function () use ($script, $type) {
-                if (!config('app.debug')) {
-                    $script = str($script)->replaceLast('.js', '.min.js')->toString();
-                    $script = str($script)->replaceLast('.css', '.min.css')->toString();
-                }
+                $id = request()->query('id');
+                $script = str($script)->replaceLast('.js', '-'.$id.'.js')->replaceLast('.css', '-'.$id.'.css')->toString();
 
-                return response()->file(__DIR__.'/../../dist/'.$script, [
+                return response()->file(__DIR__.'/../../dist/assets/'.$script, [
                     'Content-Type' => $type,
                     'Cache-Control' => 'public, max-age=31536000, immutable',
                 ]);
