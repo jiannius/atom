538d471573289b0fe8e91f5e24fe47d4175207df
TJ <tj@jiannius.com>
2026-03-19 12:00:38 +0800

    fix: added dd group cols

--- components/dd/group.blade.php
+++ components/dd/group.blade.php
@@ -1,3 +1,11 @@
-<div {{ $attributes->class(['space-y-3']) }} data-atom-dd-group>
+@props([
+    'cols' => 1,
+])
+
+<div {{ $attributes->class([match ((int) $cols) {
+    1 => 'space-y-3',
+    2 => 'grid md:grid-cols-2 gap-2',
+    3 => 'grid md:grid-cols-3 gap-2',
+}]) }} data-atom-dd-group>
     {{ $slot }}
 </div>
