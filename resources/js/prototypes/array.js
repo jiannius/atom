34ae14eeefff0a20b214b2752639447ae1efb51a
TJ <tj@jiannius.com>
2026-04-08 14:40:35 +0800

    fix: add file-minus icon component and update array prototype methods

--- resources/js/prototypes/array.js
+++ resources/js/prototypes/array.js
@@ -1,62 +1,70 @@
-Array.prototype.pluck = function (attr) {
-    if (!Array.isArray(this)) return this
-
-    return this.map(val => (val[attr]))
-}
-
-Array.prototype.unique = function (attr = null) {
-    if (!Array.isArray(this)) return this
-
-    if (typeof attr === 'function') {
-        let values = this.map(row => (attr(row)))
-
-        return [...new Set(values)].map(val => (
-            this.find(row => (attr(row) == val))
-        ))
+Object.defineProperty(Array.prototype, 'pluck', {
+    enumerable: false,
+    value: function (attr) {
+        if (!Array.isArray(this)) return this
+        return this.map(val => (val[attr]))
     }
-    else if (attr) {
-        const values = this
-            .map(row => (row[attr]))
-            .map(val => (this.find(row => (row[attr] == val))))
+})
 
-        return [...new Set(values)]
+Object.defineProperty(Array.prototype, 'unique', {
+    enumerable: false,
+    value: function (attr = null) {
+        if (!Array.isArray(this)) return this
+
+        if (typeof attr === 'function') {
+            let values = this.map(row => (attr(row)))
+            return [...new Set(values)].map(val => (
+                this.find(row => (attr(row) == val))
+            ))
+        }
+        else if (attr) {
+            const values = this
+                .map(row => (row[attr]))
+                .map(val => (this.find(row => (row[attr] == val))))
+            return [...new Set(values)]
+        }
+
+        return [...new Set([...this])]
     }
+})
 
-    return [...new Set([...this])]
-}
+Object.defineProperty(Array.prototype, 'sum', {
+    enumerable: false,
+    value: function (attr = null) {
+        if (!Array.isArray(this)) return this
 
-Array.prototype.sum = function (attr = null) {
-    if (!Array.isArray(this)) return this
+        return this.reduce((acc, value) => {
+            if (typeof attr === 'function') value = attr(value)
+            else if (attr) value = value[attr]
+            return +value + acc
+        }, 0)
+    }
+})
 
-    return this.reduce((acc, value) => {
-        if (typeof attr === 'function') value = attr(value)
-        else if (attr) value = value[attr]
+Object.defineProperty(Array.prototype, 'where', {
+    enumerable: false,
+    value: function (key, value) {
+        if (!Array.isArray(this)) return this
+        return this.filter(item => (item[key] === value))
+    }
+})
 
-        return +value + acc
-    }, 0)
-}
+Object.defineProperty(Array.prototype, 'firstWhere', {
+    enumerable: false,
+    value: function (key, value) {
+        if (!Array.isArray(this)) return this
+        let index = this.findIndex(item => (item[key] === value))
+        return this[index]
+    }
+})
 
-Array.prototype.where = function (key, value) {
-    if (!Array.isArray(this)) return this
-
-    return this.filter(item => (item[key] === value))
-}
-
-Array.prototype.firstWhere = function (key, value) {
-    if (!Array.isArray(this)) return this
-
-    let index = this.findIndex(item => (item[key] === value))
-
-    return this[index]
-}
-
-Array.prototype.toggle = function (value) {
-    if (!Array.isArray(this)) return this
-
-    const index = this.indexOf(value)
-
-    if (index === -1) this.push(value)
-    else this.splice(index, 1)
-
-    return this
-}
+Object.defineProperty(Array.prototype, 'toggle', {
+    enumerable: false,
+    value: function (value) {
+        if (!Array.isArray(this)) return this
+        const index = this.indexOf(value)
+        if (index === -1) this.push(value)
+        else this.splice(index, 1)
+        return this
+    }
+})
