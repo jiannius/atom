b5b813fe174135edd88a04fc806f4216e3c3d583
TJ <tj@jiannius.com>
2026-04-18 17:43:45 +0800

    fix: added icon in checkbox

--- components/checkbox/index.blade.php
+++ components/checkbox/index.blade.php
@@ -34,15 +34,17 @@ $merges = ['name' => $name];
                     'size-4.5 rounded-md flex items-center justify-center focus:outline-none',
                     'bg-white dark:bg-white/10',
                     'border border-zinc-300 dark:border-white/10',
-                    'text-zinc-300 dark:text-transparent',
+                    'text-zinc-300 dark:text-black',
                     'peer-checked:border-zinc-800 peer-checked:shadow-sm peer-checked:*:opacity-100',
                     'dark:peer-checked:border-white',
                     'group-has-[.error]/checkbox:outline-1 group-has-[.error]/checkbox:outline-red-500 group-has-[.error]/checkbox:outline-offset-1',
                 ])>
-                    <div class="size-3 opacity-0 rounded bg-zinc-700 dark:bg-white"></div>
+                    <div class="size-3 opacity-0 rounded bg-zinc-700 dark:bg-white flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="size-2.5" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+                    </div>
                 </div>
             </div>
-
+            
             @if ($slot->isNotEmpty())
                 <div class="dark:text-white">
                     {{ $slot }}
